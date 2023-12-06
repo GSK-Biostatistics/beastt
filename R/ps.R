1b4ad0a033971a40590b909dd275313f4507d50b
statasarus <55274484+statasaurus@users.noreply.github.com>
2023-12-06 11:10:02 +0000

    Add plots and clear-up imports

--- R/ps.R
+++ R/ps.R
@@ -7,7 +7,7 @@
 #' @param id_col Name of the column in both datasets used to identify each
 #'   subject. It must be the same across datasets
 #' @param model Model used to calculate propensity scores
-#' @param ...
+#' @param ... Optional arguments
 #'
 #' @return `prop_scr_obj` object, with the internal and the external data and
 #'   the propensity score and inverse probability weight calculated for each
@@ -17,6 +17,9 @@
 #' @importFrom stringr str_split_1
 #' @importFrom cli cli_abort
 #' @importFrom purrr reduce
+#' @importFrom rlang sym
+#' @importFrom dplyr mutate filter
+#' @importFrom stats glm
 create_prop_scr <- function(internal_df, external_df,
                             id_col, model, ...){
   if(!is_formula(model)){
@@ -84,43 +87,175 @@ create_prop_scr <- function(internal_df, external_df,
 
   all_df_ps <- all_df |>
     mutate(`___ps___` = glm(model, data = all_df, family = binomial)$fitted,
-           `___ipw___` = `___internal___` + (1 - `___internal___`) * `___ps___` / (1 - `___ps___`)
+           `___ipw___` = .data$`___internal___` + (1 - .data$`___internal___`) * .data$`___ps___` / (1 - .data$`___ps___`)
     )
 
   structure(
     list(
-         internal_df = filter(all_df_ps, `___internal___` == TRUE),
-         external_df = filter(all_df_ps, `___internal___` == FALSE),
+         internal_df = filter(all_df_ps, .data$`___internal___` == TRUE),
+         external_df = filter(all_df_ps, .data$`___internal___` == FALSE),
          id_col=id_col_en, model = model
          ),
-    class = c("prop_scr_obj")
+    class = c("prop_scr")
   )
 }
 
 #' @export
-#' @importFrom cli cli_h1 cli_text
-print.prop_scr_obj <- function(x, n = 10){
+#' @importFrom cli cli_h1 cli_text cli_bullets
+#' @importFrom dplyr select
+print.prop_scr <- function(x, ..., n = 10){
   # cat the model
   cli_h1("Model")
   cli_bullets(c("*" = f_rhs(x$model)))
   cli_h1("Propensoity Scores and Weights")
   x$external_df |>
-    select(!!x$id_col, `Propensity Score` = `___ps___`,
-           `Inverse Probablity Weight` = `___ipw___`) |>
+    select(!!x$id_col, `Propensity Score` = .data$`___ps___`,
+           `Inverse Probablity Weight` = .data$`___ipw___`) |>
     print(n = n)
 }
 
 #' @export
-tidy.prop_scr_obj <- function(x){
+tidy.prop_scr <- function(x, ...){
   x$external_df |>
-    select(!!x$id_col, `___ps___`, `___ipw___`)
+    select(!!x$id_col, .data$`___ps___`, .data$`___ipw___`)
 }
 
 #' @export
-glance.prop_scr_obj <- function(x){
+glance.prop_scr <- function(x, ...){
   x$external_df |>
     bind_rows(x$internal_df)
 }
 
-# TODO add plots
+
+#' Test If Propensity Score Object
+#'
+#' @param x Object to test
+#'
+#' @return Boolean
+#' @export
+is_prop_scr <- function(x){
+  inherits(x, "prop_scr")
+}
+
+#' Testing prop_scr
+#'
+#' Internal function what will return an error if the object is not a propensity
+#' score
+#'
+#' @param x Object to test
+#'
+#' @return NULL
+#' @noRd
+#' @keywords internal
+#' @importFrom cli cli_abort
+test_prop_scr <- function(x){
+  if(!is_prop_scr(x)){
+    cli_abort(c("{.arg x} is not a `prop_scr` object",
+                "i" = "Please use {.fun create_prop_scr} to make a `prop_scr` object"))
+  }
+}
+
+
+#' Histogram of the Propensity Score Object
+#'
+#' @param x Propensity score object
+#' @param variable Variable to plot, it must be either a propensity score or
+#'   inverse probability weight TODO MAKE THIS CLEARER
+#' @param ... Optional options for `geom_histogram`
+#'
+#' @return ggplot object
+#' @export
+#' @importFrom ggplot2 ggplot aes geom_histogram labs scale_fill_manual ggtitle
+#'   scale_x_continuous theme_bw
+#' @importFrom dplyr bind_rows
+#' @importFrom stringr str_glue
+prop_scr_hist <- function(x, variable = c("propensity score", "ps", "inverse probablity weight", "ipw"),
+                          ...){
+  test_prop_scr(x)
+
+  plot_var <- match.arg(variable)
+  x_var <- ifelse(plot_var %in% c("propensity score", "ps"),
+                          sym('___ps___'),
+                          sym('___ipw___'))
+  x_label <- ifelse(plot_var %in% c("propensity score", "ps"),
+                     "Propensity Score",
+                     "Inverse Probablity Weight")
+
+  if(plot_var %in% c("propensity score", "ps")) {
+    .data <- bind_rows(x$internal_df, x$external_df)
+  } else {
+    .data <- x$external_df
+  }
+
+  plot <-   .data|>
+    ggplot(aes(x = !!x_var, fill = .data$`___internal___`)) +
+    labs(y = "", x = x_label, fill = "Dataset") +
+    scale_fill_manual(values = c("#FFA21F", "#5398BE"),
+                      labels = c("TRUE" =  "Internal", "FALSE" = "External")) +
+    ggtitle(str_glue("Histogram of {x_label}s")) +
+    theme_bw()
+
+  if(length(list(...)) == 0) {
+    plot <- plot +
+      geom_histogram(position = "identity", binwidth = .05)
+  } else {
+    plot <- plot +
+      geom_histogram(position = "identity", ...)
+  }
+
+  plot
+
+}
+
+
+#' Density of the Propensity Score Object
+#'
+#' @param x Propensity score object
+#' @param variable Variable to plot, it must be either a propensity score or
+#'   inverse probability weight TODO MAKE THIS CLEARER
+#' @param ... Optional options for `geom_density`
+#'
+#' @return ggplot object
+#' @export
+#' @importFrom ggplot2 ggplot aes geom_density labs scale_fill_manual ggtitle scale_x_continuous
+#'   theme_bw
+#' @importFrom dplyr bind_rows
+#' @importFrom stringr str_glue
+prop_scr_dens <- function(x, variable = c("propensity score", "ps", "inverse probablity weight", "ipw"),
+                          ...){
+  test_prop_scr(x)
+
+  plot_var <- match.arg(variable)
+  x_var <- ifelse(plot_var %in% c("propensity score", "ps"),
+                  sym('___ps___'),
+                  sym('___ipw___'))
+  x_label <- ifelse(plot_var %in% c("propensity score", "ps"),
+                    "Propensity Score",
+                    "Inverse Probablity Weight")
+
+  if(plot_var %in% c("propensity score", "ps")) {
+    .data <- bind_rows(x$internal_df, x$external_df)
+  } else {
+    .data <- x$external_df
+  }
+
+  plot <-   .data |>
+    ggplot(aes(x = !!x_var, fill = .data$`___internal___`)) +
+    labs(y = "", x = x_label, fill = "Dataset") +
+    scale_fill_manual(values = c("#FFA21F", "#5398BE"),
+                      labels = c("TRUE" =  "Internal", "FALSE" = "External")) +
+    ggtitle(str_glue("Density of {x_label}s")) +
+    theme_bw()
+
+  if(length(list(...)) == 0) {
+    plot <- plot +
+      geom_density(alpha = 0.8)
+  } else {
+    plot <- plot +
+      geom_density(...)
+  }
+
+  plot
+
+}
 
---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+globalVariables(c("binomial", ".data"))
+
+
+#' @importFrom generics glance
+#' @export
+generics::glance
+
+#' @importFrom generics tidy
+#' @export
+generics::tidy
+
