3987acc5c92aa6343a1f32ca83c9e98af0a52a76
ShmoopyE <lnmavrop@gmail.com>
2024-04-24 11:01:48 +0100

    update Roxygen header

--- R/outputs.R
+++ R/outputs.R
@@ -26,21 +26,19 @@
 #' @importFrom stats glm
 #' @importFrom cobalt bal.tab
 #' @examples
+#' require(extraDistr)
 #' df <- data.frame(
-#' id_col = 1:40,
-#' cov1 = rep(c("a", "b", "c", "d"), 10),
-#' cov2 = rep(c("e", "f", "g", "h"), each = 10),
-#' internal = rdunif(40, 0, 1),
-#' trt = rep(c("Active", "Control"), each = 20)
+#'   id_col = 1:40,
+#'   cov1 = rep(c("a", "b", "c", "d"), 10),
+#'   cov2 = rep(c("e", "f", "g", "h"), each = 10),
+#'   internal = rdunif(40, 0, 1),
+#'   trt = rep(c("Active", "Control"), each = 20)
 #' )
-#'
 #' model <- as.formula("~cov1 + cov2")
-#'
 #' internal_df <- df %>%
 #' filter(internal == 1)
 #' external_df <- df %>%
 #' filter(internal == 0)
-#'
 #' pre_post_summary(df, vars = c("cov1", "cov2"), df$internal, trt = df$trt,
 #' internal_df, external_df, model = model, id_col = id_col)
 pre_post_summary <- function(df, vars, internal, trt, internal_df,
