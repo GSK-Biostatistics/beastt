a58ca7678a4d3056606a9c5ab99083d454c5dfa2
statasarus <55274484+statasaurus@users.noreply.github.com>
2024-06-05 10:57:53 +0100

    Add functionality for normal known and unknown SD

--- R/post.R
+++ R/post.R
@@ -3,8 +3,8 @@
 #' @param internal_data This can either be a propensity score object or a tibble
 #'   of the internal data.
 #' @param response Name of response variable
-#' @param internal_control_sd Assumed known SD of the internal control
 #' @param prior distributional object, if you would like a mixture distribution
+#' @param internal_control_sd Assumed known SD of the internal control
 #'
 #' @return distributional object
 #' @export
@@ -15,8 +15,8 @@
 calc_post_norm<- function(
     internal_data,
     response,
-    internal_control_sd,
-    prior
+    prior,
+    internal_control_sd = NULL
 ){
   # Checking internal data and response variable
   if(is_prop_scr(internal_data)){
@@ -38,9 +38,6 @@ calc_post_norm<- function(
   if(!is.null(check$error)){
     cli_abort("{.agr response} was not found in {.agr internal_data}")
   }
-  # Sum of responses in internal control arm
-  sum_resp <- pull(data, !!response) |>
-    sum()
 
   # Checking the dirstibution and getting the family
   if(!is_distribution(prior)){
@@ -48,18 +45,34 @@ calc_post_norm<- function(
   }
   prior_fam <- family(prior)
 
-  if(prior_fam == "normal"){
-    out_dist <- calc_norm_post(prior, internal_control_sd, nIC, sum_resp)
-  } else if(prior_fam == "student_t") {
-    out_dist <- t_to_mixnorm(prior) |>
-      calc_mixnorm_post(internal_control_sd, nIC, sum_resp)
-  } else if(prior_fam == "mixture") {
-    dist_ls <- get_base_families(prior)
-
-    out_dist <- calc_mixnorm_post(prior, internal_control_sd, nIC, sum_resp)
+  # With known control SD
+  if(!is.null(internal_control_sd)) {
+    # Sum of responses in internal control arm
+    sum_resp <- pull(data, !!response) |>
+      sum()
+    if(prior_fam == "normal"){
+      out_dist <- calc_norm_post(prior, internal_control_sd, nIC, sum_resp)
+    } else if (prior_fam == "student_t") {
+      out_dist <- t_to_mixnorm(prior) |>
+        calc_mixnorm_post(internal_control_sd, nIC, sum_resp)
+    } else if(prior_fam == "mixture") {
+      out_dist <- mix_t_to_mix(prior) |>
+        calc_mixnorm_post(internal_control_sd, nIC, sum_resp)
+    } else {
+      cli_abort("{.agr prior} must be either normal, t, or a mixture of normals")
+    }
   } else {
-    cli_abort("{.agr prior} must be either normal, t, or a mixture of normals and t")
+    if(prior_fam == "student_t") {
+      out_dist <- t_to_mixnorm(prior) |>
+        calc_t_post(nIC, pull(data, !!response))
+    } else if(prior_fam == "mixture") {
+      out_dist <- mix_t_to_mix(prior) |>
+        calc_t_post(nIC, pull(data, !!response))
+    } else {
+      cli_abort("{.agr prior} must be either normal, t, or a mixture of normals and t")
+    }
   }
+
   out_dist
 }
 
@@ -70,6 +83,7 @@ calc_post_norm<- function(
 #' @return String of families
 #' @noRd
 #' @importFrom purrr quietly
+#' @importFrom stats quantile
 t_to_mixnorm <- function(x){
   # distribution is constrained to equal the mean of the t distribution
   # quantiles of t distribution
@@ -78,9 +92,9 @@ t_to_mixnorm <- function(x){
     unlist()
 
   normEM_obj <- quietly(normalmixEM)(t_qntl,
-              k = 2,                            # number of normal components
-              mean.const = c(mean(x), mean(x)), # constraints on the means
-              maxit = 1000)$result                     # maximum number of iterations
+                                     k = 2,                            # number of normal components
+                                     mean.const = c(mean(x), mean(x)), # constraints on the means
+                                     maxit = 1000)$result                     # maximum number of iterations
 
   norm_mix_w <- normEM_obj$lambda       # 2 x 1 vector of weights associated with each normal component
   norm_mix_mu <- normEM_obj$mu          # 2 x 1 vector of means associated with each normal component
@@ -89,6 +103,43 @@ t_to_mixnorm <- function(x){
   dist_mixture(!!!norm_ls, weights = c(norm_mix_w[1], 1-norm_mix_w[1]))
 }
 
+
+#' Takes a mixture distribution, checks if there is any t distributions and changes them to a mixture of normals
+#'
+#' @param x
+#'
+#' @return mixture distributional object
+#' @importFrom distributional new_dist
+#' @noRd
+mix_t_to_mix <- function(x){
+  dists <- get_base_families(x) |> unlist()
+  if(!all(dists %in% c("normal", "student_t"))){
+    cli_abort("Mixtures must be only made up of normal and t distributions")
+  } else if("student_t" %in% dists){
+    t_check <- TRUE
+    while(t_check){
+      i<-which(dists == "student_t")[1]
+      new_mix <- parameters(x)$dist[[1]][[i]] |>
+        t_to_mixnorm()
+      old_weight <- parameters(x)$w[[1]][i]
+      new_weights <- parameters(new_mix)$w[[1]]*old_weight
+
+      #Removing the old mixture from lost of distributions and adding the new one in
+      new_dist_ls <- c(parameters(x)$dist[[1]][-i],
+                       parameters(new_mix)$dist[[1]]) |>
+        # This converts the list back to distributional objects
+        map(function(dist){
+          new_dist(!!!parameters(dist), class=class(dist)[1])
+        })
+      # This is to make sure it adds to 1
+      new_weight_vec <- c(parameters(x)$w[[1]][-i], new_weights[1],
+                          1-sum(parameters(x)$w[[1]][-i], new_weights[1]))
+      x <- dist_mixture(!!!new_dist_ls, weights = new_weight_vec)
+      t_check <- "student_t" %in% unlist(get_base_families(x))
+    }
+  }
+  x
+}
 
 
 #' @param prior mixture prior
@@ -99,12 +150,8 @@ t_to_mixnorm <- function(x){
 #' @noRd
 calc_mixnorm_post <- function(prior,internal_control_sd,  n_ic, sum_resp){
 
-  prior_means <- parameters(prior)$dist[[1]]|>
-    map(\(x) x$mu) |>
-    unlist()
-  prior_sds <- parameters(prior)$dist[[1]]|>
-    map(\(x) x$sigma) |>
-    unlist()
+  prior_means <- mix_means(prior)
+  prior_sds <- mix_sigmas(prior)
   prior_ws <- parameters(prior)$w[[1]]
 
   # K x 1 vectors of means and SDs of each component of posterior distribution for mu_C
@@ -139,7 +186,7 @@ calc_norm_post <- function(prior,internal_control_sd, n_ic, sum_resp){
   x <- parameters(prior)
 
   # K x 1 vectors of means and SDs of each component of posterior distribution for mu_C
-  post_sds <- (nIC/internal_control_sd^2 + 1/x$sigma^2)^-.5        # vector of SDs
+  post_sds <- (n_ic/internal_control_sd^2 + 1/x$sigma^2)^-.5        # vector of SDs
   post_means <- post_sds^2 * (sum_resp/internal_control_sd^2 + x$mu/x$sigma^2)   # vector of means
   final_dist <- dist_normal(post_means, post_sds)
 }
@@ -160,3 +207,58 @@ get_base_families <- function(x) {
   fam
 }
 
+calc_t_post <- function(prior, nIC, response){
+  # Get the integratedlikelihood and convert it to a mixture of normals
+  pi_tilde_IC <- calc_t(response, n= nIC)
+  int_like <- t_to_mixnorm(pi_tilde_IC)
+  like_mean <- mix_means(int_like)
+  like_sds <- mix_sigmas(int_like)
+  like_ws <- parameters(int_like)$w[[1]]
+
+  prior_means <- mix_means(prior)
+  prior_sds <- mix_sigmas(prior)
+  prior_ws <- parameters(prior)$w[[1]]
+
+  # 2*K x 1 vectors of means and SDs of each normal component of posterior distribution for mu_C
+  K <- length(prior_means)
+  post_sds <- c((1/like_sds[1]^2 + 1/prior_sds^2)^-.5,    # vector of SDs
+                    (1/like_sds[2]^2 + 1/prior_sds^2)^-.5)
+  post_means <- post_sds^2 *     # vector of means
+    c((like_mean[1]/like_sds[1]^2 + prior_means/prior_sds^2),
+      (like_mean[2]/like_sds[2]^2 + prior_means/prior_sds^2))
+
+  # Create a list of normal distributions
+  post_ls <- map2(post_means, post_sds, function(mu, sigma){
+    dist_normal(mu, sigma)
+  })
+
+  # 2*K x 1 vector of posterior weights (unnormalized) corresponding to each component of the
+  # posterior distribution for mu_C
+  log_post_w_propto <-
+    c(log(prior_ws) + log(like_ws[1]) - .5*log(2*pi * like_sds[1]^2 * prior_sds^2 * post_sds[1:K]^-2) -
+        .5 * like_mean[1]^2/like_sds[1]^2 - .5 * prior_means^2/prior_sds^2 +
+        .5 * post_means[1:K]^2 * post_sds[1:K]^-2,
+      log(prior_ws) + log(like_ws[2]) - .5*log(2*pi * like_sds[2]^2 * prior_sds^2 * post_sds[(K+1):(2*K)]^-2) -
+        .5 * like_mean[2]^2/like_sds[2]^2 - .5 * prior_means^2/prior_sds^2 +
+        .5 * post_means[(K+1):(2*K)]^2 * post_sds[(K+1):(2*K)]^-2)
+
+  # 2*K x 1 vector of posterior weights (normalized) corresponding to each component of the
+  # posterior distribution for mu_C
+  adj_log_post_w_propto <- exp(log_post_w_propto - max(log_post_w_propto))  # subtract max of log weights before exponentiating
+  post_w_norm <- adj_log_post_w_propto / sum(adj_log_post_w_propto)
+  sum_fixed_w <- c(1-sum(post_w_norm[-1]), post_w_norm[-1])
+  dist_mixture(!!!post_ls, weights = sum_fixed_w)
+
+}
+
+mix_means <- function(x){
+  parameters(x)$dist[[1]]|>
+    map(\(dist) dist$mu) |>
+    unlist()
+}
+
+mix_sigmas <- function(x){
+  parameters(x)$dist[[1]]|>
+    map(\(dist) dist$sigma) |>
+    unlist()
+}
