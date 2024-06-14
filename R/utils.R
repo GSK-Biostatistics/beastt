3fdf32a6fbd2a803cfe9fab2644cfee62ab3117c
statasarus <christinaefillmore@gmail.com>
2024-06-14 14:54:47 +0100

    #29 Also can be named

--- R/utils.R
+++ R/utils.R
@@ -8,13 +8,24 @@
 #' @importFrom distributional is_distribution
 #' @importFrom ggplot2 ggplot theme_bw
 #' @importFrom ggdist stat_slabinterval
-plot_dist <- function(distribution){
-  if(!is_distribution(distribution)){
-    cli_abort("Must be given a distributional object")
+plot_dist <- function(...){
+  input <- list(...)
+  if(!all(purrr::map_lgl(input, is_distribution))){
+    cli_abort("Must be given distributional objects")
   }
 
-  ggplot(data.frame(), aes(xdist = distribution, y =1)) +
-    stat_slabinterval() +
+  Distributions <- names(input)
+  n <- length(input)
+  fill_alpha <- ifelse(n > 1, 0.5, 1)
+  if(is.null(Distributions) & n > 1){
+    Distributions <- purrr::map_chr(input, format)
+    if(unique(Distributions) != n)
+      Distributions <- paste0(1:n,": ", Distributions)
+
+  }
+  ggplot(data.frame(), aes(xdist = input, fill = Distributions)) +
+    stat_slabinterval(alpha = fill_alpha) +
+    labs(y = "Density", x = "") +
     theme_bw()
 
 }
