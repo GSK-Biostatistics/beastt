de9dc67bd86379542e64003ff51336815e7e0316
statasarus <christinaefillmore@gmail.com>
2024-06-14 15:10:47 +0100

    Update documentation and removing old code

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 #' Title
 #'
-#' @param distribution Distributional object to plot
+#' @param ... Distributional object(s) to plot. If there are multiple objects you can name them.
 #'
 #' @return ggplot object that is the density of the provided distribution
 #' @export
@@ -8,9 +8,10 @@
 #' @importFrom distributional is_distribution
 #' @importFrom ggplot2 ggplot theme_bw
 #' @importFrom ggdist stat_slabinterval
+#' @importFrom purrr map_chr map_lgl
 plot_dist <- function(...){
   input <- list(...)
-  if(!all(purrr::map_lgl(input, is_distribution))){
+  if(!all(map_lgl(input, is_distribution))){
     cli_abort("Must be given distributional objects")
   }
 
@@ -18,7 +19,7 @@ plot_dist <- function(...){
   n <- length(input)
   fill_alpha <- ifelse(n > 1, 0.5, 1)
   if(is.null(Distributions) & n > 1){
-    Distributions <- purrr::map_chr(input, format)
+    Distributions <- map_chr(input, format)
     if(unique(Distributions) != n)
       Distributions <- paste0(1:n,": ", Distributions)
 
