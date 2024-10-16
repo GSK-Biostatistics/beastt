8197cd4f5b0da72e41ef41c7c6fa6900f2fabaa0
abiterry <abs2001@hotmail.co.uk>
2024-10-16 16:16:09 +0100

    Resolve issue #55

--- R/app.R
+++ R/app.R
@@ -1,4 +1,4 @@
-#' BDB Template Maker
+#' BDB Code Template Maker
 #'
 #' RStudio add-in to create template BDB code
 #'
@@ -8,58 +8,56 @@
 #'@import miniUI
 #'@import bslib
 #'@importFrom rstudioapi documentNew
-bdb_template_maker <- function(){
+bdb_code_template_maker <- function(){
   # Define UI for application
-  ui <- fluidPage(
+  ui <- page_sidebar(
     id = "bdb_template_app",
     theme = bs_theme(bootswatch = "united",
                      primary = "#F36633",
                      secondary = "#f39633",
                      "navbar-bg" = "#F36633"
     ),
-    titlePanel("BDB Template Maker"),
-    sidebarLayout(
-      sidebarPanel(
-        h3("Inputs"),
-        shiny::radioButtons("Purpose", "Purpose",
-                            choices = c("Analysis", "Simulation")),
-        h3("Study"),
-        shiny::selectInput("endPoint", "Endpoint Type",
-                           choices=c("Binary", "Normal", "Survival")),
-        shiny::numericInput("ssIntArm", "Sample Size Internal Arm:", value = NULL),
-        shiny::numericInput("RMPWeights", "Weights for RMP:", value = NULL),
-        shiny::actionButton(inputId="submit", label= "Submit")
-      ),
-      mainPanel(
-        h3("Data"),
-        shiny::checkboxGroupInput(
-          inputId = "dataChks",
-          label = "",
-          choices = c("External Data",
-                      "Differing Sample Size per Arm",
-                      "Estimate 'true' Covariate Effects",
-                      "Underlying SD (Normal Case)",
-                      "Robustify Power Prior")
+    title ="BDB Code Template Maker",
+    sidebar=sidebar(
+      h3("Inputs"),
+      shiny::radioButtons("Purpose", "Purpose",
+                          choices = c("Analysis", "Simulation")),
+      h3("Study"),
+      shiny::selectInput("endPoint", "Endpoint Type",
+                         choices=c("Binary", "Normal", "Survival")),
+      shiny::numericInput("ssIntArm", "Sample Size Internal Arm:", value = NULL),
+      shiny::numericInput("RMPWeights", "Weights for RMP:", value = NULL),
+      shiny::actionButton(inputId="submit", label= "Submit")
+    ),
+    card(
+      h3("Data"),
+      shiny::checkboxGroupInput(
+        inputId = "dataChks",
+        label = "",
+        choices = c("External Data",
+                    "Differing Sample Size per Arm",
+                    "Estimate 'true' Covariate Effects",
+                    "Underlying SD (Normal Case)",
+                    "Robustify Power Prior")
 
-        ),
-        shiny::radioButtons("covDatGen",
-                            label = "Covariate Data Generation",
-                            choices = c("External Data Based (bootstrap)",
-                                        "Assumed known covariance",
-                                        "Other")),
-        shiny::selectInput("borrType", "Type of Borrowing",
-                           choices=c("On control arm",
-                                     "On treatment arm",
-                                     "No borrowing")),
-        shiny::checkboxGroupInput(
-          inputId = "Plots",
-          label = "Output plots",
-          choices=c("Histogram of Propensity Scores",
-                    "Density of Inverse Probability Weights",
-                    "Covariates Balance",
-                    "Prior Distribution",
-                    "Posterior Distributions")
-        )
+      ),
+      shiny::radioButtons("covDatGen",
+                          label = "Covariate Data Generation",
+                          choices = c("External Data Based (bootstrap)",
+                                      "Assumed known covariance",
+                                      "Other")),
+      shiny::selectInput("borrType", "Type of Borrowing",
+                         choices=c("On control arm",
+                                   "On treatment arm",
+                                   "No borrowing")),
+      shiny::checkboxGroupInput(
+        inputId = "Plots",
+        label = "Output plots",
+        choices=c("Histogram of Propensity Scores",
+                  "Density of Inverse Probability Weights",
+                  "Covariates Balance",
+                  "Prior Distribution",
+                  "Posterior Distributions")
       )
     )
   )
