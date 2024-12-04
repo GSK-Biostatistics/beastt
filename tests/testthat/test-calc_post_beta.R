b6f7c11f76f48ed3cec519bb4676c9e3103c40c8
abiterry <abs2001@hotmail.co.uk>
2024-12-04 14:47:14 +0000

    Create test-calc_post_beta.R

---
+++ tests/testthat/test-calc_post_beta.R
@@ -0,0 +1,30 @@
+# Define test data
+set.seed(1234)
+internal_df <- data.frame(id_col = 1:20, cov1 = rnorm(10, 2), cov2 = rnorm(100, 20),
+                          trt = rbinom(20, 1, 0.5), y = rbinom(20, 1, 0.3))
+pwr_prior <- dist_beta(38, 44)
+
+# Test for valid inputs
+test_that("calc_post_beta handles valid inputs", {
+  post_beta_result <- calc_post_beta(filter(internal_df, trt==0),
+                                     response = y,
+                                     prior = pwr_prior)
+  expect_s3_class(post_beta_result, "distribution")
+})
+
+# Test for invalid external data
+test_that("calc_post_beta handles invalid internal data", {
+  expect_error(calc_post_beta(c(4, 8, 12), response = y, prior = pwr_prior))
+  expect_error(calc_post_beta("abc", response = y, prior = pwr_prior))
+})
+
+# Test for invalid prior
+test_that("calc_post_beta handles invalid prior", {
+  expect_error(calc_post_beta(filter(internal_df, trt==0), response = y, prior = 5))
+  expect_error(calc_post_beta(filter(internal_df, trt==0), response = y, prior = "a"))
+})
+
+# Test for invalid response variable
+test_that("calc_post_beta handles invalid response", {
+  expect_error(calc_post_beta(filter(internal_df, trt==0), response = invalid, prior = pwr_prior))
+})
