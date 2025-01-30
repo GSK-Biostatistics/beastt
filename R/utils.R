77400312ed3fbe3c831e2d50a03c4bdb2bd3d037
aht88385_gsk <119593461+abiterry@users.noreply.github.com>
2025-01-30 16:37:55 +0000

    require mvtnorm and move to suggests

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,35 @@
+#' Check package installation
+#'
+#' @param pkg (`character`)\cr
+#'   vector of package names to check.
+#' @param ref (`string`)\cr
+#'   name of the package the function will search for a minimum required version from.
+#' @param call (`environment`)\cr
+#'   frame for error messaging. Default is [get_cli_abort_call()].
+#' @examples
+#' check_pkg_installed("dplyr")
+#'
+#' @noRd
+check_pkg_installed <- function(pkg,
+                                ref = utils::packageName(),
+                                call = get_cli_abort_call()) {
+  if (!is.character(ref) && !is.null(ref)) cli::cli_abort("{.arg ref} must be a string.")
+
+  # get min version data -------------------------------------------------------
+  df_pkg_min_version <-
+    get_min_version_required(pkg = pkg, ref = ref)
+
+  # prompt user to install package ---------------------------------------------
+  rlang::check_installed(
+    pkg = df_pkg_min_version$pkg,
+    version = df_pkg_min_version$version,
+    compare = df_pkg_min_version$compare,
+    call = call
+  ) |>
+    # this can be removed after this issue is resolved https://github.com/r-lib/rlang/issues/1694
+    suppressWarnings()
+}
+
 #' Plot Distribution
 #'
 #' @param ... Distributional object(s) to plot. When passing multiple objects
@@ -64,6 +96,7 @@ plot_dist_mvnorm <- function(dist_list){
   if(!all(map_lgl(dist_list, is_mvnorm))){
     cli_abort("Must be given multivariate normal objects")
   } else {
+    check_pkg_installed("mvtnorm")
     tib <- data.frame(x1=numeric(), x2=numeric(), z=numeric(), id=factor())
     for (i in 1:length(dist_list)){
       dist <- dist_list[[i]]
